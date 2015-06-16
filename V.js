eaf7b242f002892bf2ca01d0071ff2d444ee6d5c
Mikkel Madsen <m@madsn.net>
2015-06-16 23:25:33 +0200

    Fetch activities from db

--- V.js
+++ V.js
@@ -1,27 +1,39 @@
+Activities = new Meteor.Collection('activities');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  Meteor.subscribe('activities');
 
   Template.activitycard.helpers({
-    activities: [
-      {
-        name: "Pool",
-        img: "http://i.imgur.com/JN1Bgld.jpg"
-      },
-      {
-        name: "Tabletennis",
-        img: "http://i.imgur.com/iGB9C24.jpg"
-      },
-      {
-        name: "Foosball",
-        img: "http://i.imgur.com/pD6PNLE.jpg"
-      }
-    ]
-  })
+    activities: function() {
+      return Activities.find();
+    },
+    count: function() {
+      return Activities.find().count();
+    }
+  });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    if (Activities.find().count() === 0){
+      Activities.insert({
+          name: "Pool",
+          img: "http://i.imgur.com/JN1Bgld.jpg"
+        });
+      Activities.insert({
+          name: "Tabletennis",
+          img: "http://i.imgur.com/iGB9C24.jpg"
+        });
+      Activities.insert({
+          name: "Foosball",
+          img: "http://i.imgur.com/pD6PNLE.jpg"
+        });
+    }
   });
+
 }
+
+Router.route('/', function () {
+  this.render('Home');
+});
