9d1a75d555120c7749906522bfc073e2168050c6
Mikkel Madsen <m@madsn.net>
2015-07-02 08:13:27 +0200

    Replace orion admin with houston

--- both/1-collections.js
+++ both/1-collections.js
@@ -1,44 +1,6 @@
-Activities = new orion.collection('activities', {
-  singularName: 'activity',
-  pluralName: 'activities',
-  link: {
-    title: 'Activities'
-  },
-  tabular: {
-    columns: [
-      { data: "name", title: "Name" }
-    ]
-  }
-});
-Players = new orion.collection('players', {
-  singularName: 'player',
-  pluralName: 'players',
-  link: {
-    title: 'Players'
-  },
-  tabular: {
-    columns: [
-      { data: "activity", title: "Activity" },
-      { data: "rank", title: "Rank" },
-      { data: "user", title: "User" }
-    ]
-  }
-});
-Challenges = new orion.collection('challenges', {
-  singularName: 'challenge',
-  pluralName: 'challenges',
-  link: {
-    title: 'Challenges'
-  },
-  tabular: {
-    columns: [
-      { data: "challenger", title: "Challenger" },
-      { data: "opponent", title: "Opponent" },
-      { data: "activity", title: "Activity" },
-      orion.attributeColumn('createdAt', 'createdAt', 'Created At')
-    ]
-  }
-});
+Activities = new Meteor.Collection('activities');
+Players = new Meteor.Collection('players');
+Challenges = new Meteor.Collection('challenges');
 
 Activities.attachSchema(new SimpleSchema({
   name: {
@@ -70,6 +32,5 @@ Challenges.attachSchema(new SimpleSchema({
   },
   opponent: {
     type: String
-  },
-  createdAt: orion.attribute('createdAt')
+  }
 }));
